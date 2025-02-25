74e4c24d3be3bb27b509c4cb9e2522bd86f34bdd
aidan <aidan@ro-des.com>
2025-02-25 10:29:38 +0100

    Se ha creado la funcionalidad de que se puedan enviar los contactos de la base de datos al correo + guardar y cargar estilos de la base de datos

--- connection.py
+++ connection.py
@@ -3,3 +3,6 @@ import pymongo
 connection = pymongo.MongoClient("mongodb+srv://root:Aidan9919.@cluster0.nywpd.mongodb.net/")
 database = connection["agenda"]
 columna = database["contactos"]
+
+database_2 = connection['estilos']
+columna_2 = database_2['estilos']
