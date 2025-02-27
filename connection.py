c53a38c84bdfb8944a31f2aa5775a4138ad98187
aidan <aidan@ro-des.com>
2025-02-27 12:14:35 +0100

    Cambios en connection.py: ahora se juntan las 3 colecciones en una sola base de datos en vez de tener 3 bases con 1 colección cada una

--- connection.py
+++ connection.py
@@ -2,10 +2,7 @@ import pymongo
 
 connection = pymongo.MongoClient("mongodb+srv://root:Aidan9919.@cluster0.nywpd.mongodb.net/")
 database = connection["agenda"]
+
 columna = database["contactos"]
-
-database_2 = connection['estilos']
-columna_2 = database_2['estilos']
-
-database_3 = connection['eliminados']
-columna_3 = database_3['contactos_eliminados']
+columna_2 = database['estilos']
+columna_3 = database['eliminados']
